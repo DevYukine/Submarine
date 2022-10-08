41d2fb134098aafaf270fbd2c7ed4cc58ef1ce56
Yukine <devyukine@gmx.de>
2022-10-08 23:49:39 +0200

    refactor(QualityEdgeCases): make class static

--- Submarine.Core/Quality/QualityEdgeCases.cs
+++ Submarine.Core/Quality/QualityEdgeCases.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace Submarine.Core.Quality;
 
-public class QualityEdgeCases
+public static class QualityEdgeCases
 {
 	/// <summary>
 	///     These are default Qualities of groups which do not add there Quality in the Release Group, these will only be used
