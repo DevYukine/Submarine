a38ab8c12ee134e7af7c1efe1df4a7c567e17b19
Yukine <devyukine@gmx.de>
2022-10-08 21:55:42 +0200

    feat(Api): add Release parsing route

---
+++ Submarine.Api/Controllers/ReleaseController.cs
@@ -0,0 +1,40 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Submarine.Core.Parser;
+using Submarine.Core.Provider;
+using Submarine.Core.Release;
+using Submarine.Core.Release.Torrent;
+using Submarine.Core.Release.Usenet;
+
+namespace Submarine.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReleaseController : ControllerBase
+{
+	private readonly ILogger<ReleaseController> _logger;
+	private readonly IParser<TorrentRelease> _torrentReleaseParserService;
+	private readonly IParser<UsenetRelease> _usenetReleaseParserService;
+
+	public ReleaseController(ILogger<ReleaseController> logger,
+		IParser<TorrentRelease> torrentReleaseParserService, IParser<UsenetRelease> usenetReleaseParserService)
+	{
+		_logger = logger;
+		_torrentReleaseParserService = torrentReleaseParserService;
+		_usenetReleaseParserService = usenetReleaseParserService;
+	}
+
+	[HttpGet]
+	public IActionResult GetAsync([FromQuery, Required] string title, [FromQuery, Required] Protocol protocol)
+	{
+		BaseRelease parsed = protocol switch
+		{
+			Protocol.BITTORRENT => _torrentReleaseParserService.Parse(title),
+			Protocol.USENET => _usenetReleaseParserService.Parse(title),
+			Protocol.XDCC => throw new NotImplementedException(),
+			_ => throw new ArgumentOutOfRangeException(nameof(protocol), protocol, null)
+		};
+
+		return Ok(parsed);
+	}
+}
