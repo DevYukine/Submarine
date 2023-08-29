89cc807f9d29fbc75a9aae20769c4630dc04d006
Yukine <devyukine@gmx.de>
2023-08-29 18:07:03 +0200

    feat(TorrentReleaseFlag): add NeutralLeech, Internal & Exclusive Flags

--- Submarine.Core/Release/Torrent/TorrentReleaseFlags.cs
+++ Submarine.Core/Release/Torrent/TorrentReleaseFlags.cs
@@ -13,9 +13,43 @@ public enum TorrentReleaseFlags
 	/// </summary>
 	NONE = 0,
 
+	/// <summary>
+	/// Torrent is Freeleech (0% Download Credited)
+	/// </summary>
 	FREELEECH = 1 << 1,
+
+	/// <summary>
+	/// Torrent is Halfleech (50% Download Credited)
+	/// </summary>
 	HALFLEECH = 1 << 2,
-	DOUBLE_UPLOAD = 1 << 3,
-	OTHER_PROMOTION = 1 << 4,
-	SCENE = 1 << 5
+
+	/// <summary>
+	/// No Upload/Download is counted for this Torrent
+	/// </summary>
+	NEUTRALLEECH = 1 << 3,
+
+	/// <summary>
+	/// Double Upload is counted for this Torrent
+	/// </summary>
+	DOUBLE_UPLOAD = 1 << 4,
+
+	/// <summary>
+	/// Torrent has a Promotion (anything besides Freeleech, Halfleech, Double Upload)
+	/// </summary>
+	OTHER_PROMOTION = 1 << 5,
+
+	/// <summary>
+	/// Torrent is a Scene Release
+	/// </summary>
+	SCENE = 1 << 6,
+
+	/// <summary>
+	/// Torrent is an Internal Release for this Tracker
+	/// </summary>
+	INTERNAL = 1 << 7,
+
+	/// <summary>
+	/// This Torrent is Exclusive to this Tracker
+	/// </summary>
+	EXCLUSIVE = 1 << 8,
 }
